a28ac642d795cfb95a5034713afaaddab9653311
aimfeld <adrian.imfeld@cloud-solutions.net>
2013-03-07 16:40:25 +0100

    doc

--- config/module.config.php
+++ config/module.config.php
@@ -13,19 +13,25 @@ return array(
         'scan_directories' => array(
             // e. g. 'vendor/provider/module/src',
         ),
-        // Standard ZF2 key
+        // Definitions
         'definition' => array(
+            // Add definitions which are not covered by the code scanning process. Usually, this can be omitted.
         ),
         'instance' => array(
             // Type preferences for abstract classes and interfaces.
             'preference' => array(
                 // e. g. 'MyModule/SomeInterface' => 'MyModule/SomeClass',
             ),
-            /* e. g.
+            // Add instance configuration if there are specific parameters to be used for instance creation.
+            // A good approach is to pass a Zend\Config\Config object to your classes instead of single
+            // parameters. This makes things more flexible and avoids the need of configuring instance parameters.
+            // Of course, third party modules may not follow this approach, so intances need to be configured here, e.g.
+            /*
             'MyModule/HelloWorldClass' => array('parameters' => array(
                 'helloParam' => 'Hello',
                 'worldParam' => 'World',
-            )),*/
+            )),
+            */
         ),
     ),
 );
